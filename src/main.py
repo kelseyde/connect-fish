2a537e4f2fc5d5455678fc515d435a31c95c78f0
kelseyde <daniel.kelsey@hotmail.co.uk>
2024-05-26 17:30:03 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
 print("hello world")
+
+def print_board():
+    print("""
+    - - - - - - -
+    - - - - - - -
+    - - - - - - -
+    - - - - - - -
+    - - - - - - -
+    - - - - - - -
+    """)
+
+running = True
+while (running):
+    command = input()
+    match command:
+        case "newgame": print_board();
+        case "quit": running = False
+
